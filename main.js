120359256a2bd467323038b1dc1509bf752a9b64
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-10-07 11:43:01 +0200

    styling toegevoegd

--- main.js
+++ main.js
@@ -20,6 +20,9 @@ let directions = new MapboxDirections({
 map.addControl(directions, "top-left");
 
 directions.on("route", (e) => {
+  console.log(e);
+  let minutes = e.route[0].duration / 60;
+
   let kmDistance = e.route[0].distance / 1000;
   let startRate = 5;
   let kmRate = 2.47;
@@ -33,4 +36,16 @@ directions.on("route", (e) => {
   document
     .getElementsByClassName("mapbox-directions-component")[1]
     .appendChild(tag);
+
+  let prijs = document.createTextNode(totalCosts.toFixed(2));
+  document.getElementById("prijs").innerHTML = "";
+  document.getElementById("prijs").appendChild(prijs);
+
+  let kilometers = document.createTextNode(kmDistance.toFixed(2));
+  document.getElementById("afstand").innerHTML = "";
+  document.getElementById("afstand").appendChild(kilometers);
+
+  let tijd = document.createTextNode(minutes.toFixed(0));
+  document.getElementById("tijd").innerHTML = "";
+  document.getElementById("tijd").appendChild(tijd);
 });
